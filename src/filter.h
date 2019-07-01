8e7f46e11c0b86ca6655984b1ec633cd2e2d984e
Evgenii Vasiliev <eugene.unn@gmail.com>
2019-07-01 12:27:57 +0300

    Practice1 update
    Updated practice 1 description and files

--- src/filter.h
+++ src/filter.h
@@ -10,41 +10,6 @@ using namespace std;
 
 class Filter
 {
-	public:
-	virtual Mat ProcessImage(Mat image) = 0 {}
-};
-
-class GrayFilter : Filter
-{
-private:
-
-public:
-	Mat ProcessImage(Mat image)
-	{
-		Mat result;
-		cvtColor(image, result, cv::COLOR_BGR2GRAY);
-		return result;
-	}
-	
-};
-
-class ResizeFilter : Filter
-{
-private:
-	int width;
-	int height;
-public:
-	ResizeFilter(int newWidth, int newHeight)
-	{
-			this->width = newWidth;
-			this->height = newHeight;
-	}
-	
-	Mat ProcessImage(Mat image)
-	{
-		Mat result;
-		resize(image, result, Size(width,height));
-		return result;
-	}
-	
+    public:
+    virtual Mat ProcessImage(Mat image) = 0 {}
 };
