a084996e49b6e301bf21fa5e9bee4e76a28e8d2b
EvgenyShashkin <temp2019@ccam.cmc.unn.net>
2019-07-03 16:54:48 +0300

    Practice 1

---
+++ samples/practice1_Shashkin_Evgeny.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+#include "filter.h"
+
+using namespace cv;
+using namespace std;
+
+const char* cmdAbout = "Sample of OpenCV usage. ";
+
+const char* cmdOptions =
+"{ i  image         | C:\\Users\\Public\\Pictures\\Sample Pictures\\Desert.jpg | image to process        }"
+"{ w  width         | 640 | width for image resize  }"
+"{ h  height        | 480 | height for image resize }"
+"{ q ? help usage   | <none> | print help message      }";
+
+int main(int argc, char** argv)
+{
+    // Process input arguments
+    CommandLineParser parser(argc, argv, cmdOptions);
+    parser.about(cmdAbout);
+
+    if (parser.has("help"))
+    {
+        parser.printMessage();
+        return 0;
+    }
+    if (!parser.check())
+    {
+        parser.printErrors();
+        return 0;
+    }
+    
+    // Load image
+    String imgName(parser.get<String>("image"));
+		String imgWidth(parser.get<String>("width"));
+		String imgHeight(parser.get<String>("height"));
+
+		Mat image = imread(imgName);
+    
+    // Filter image
+		GrayFilter gray;
+		Mat grayImage;
+		grayImage=gray.ProcessImage(image);
+
+		ResizeFilter resize(stoi(imgWidth), stoi(imgHeight));
+		Mat resizedImage;
+		resizedImage = resize.ProcessImage(image);
+
+    // Show image
+		namedWindow("Gray image", WINDOW_AUTOSIZE);
+		imshow("Gray image", grayImage);
+		waitKey(0);
+
+		namedWindow("Resized image", WINDOW_AUTOSIZE);
+		imshow("Resized image", resizedImage);
+		waitKey(0);
+    
+    return 0;
+}
--- src/filter.cpp
+++ src/filter.cpp
@@ -1,1 +1,24 @@
 #include "filter.h"
+
+Mat GrayFilter::ProcessImage(Mat image)
+{
+	Mat gray;
+	cvtColor(image, gray, COLOR_BGR2GRAY);
+	return gray;
+}
+
+ResizeFilter::ResizeFilter(int newWidth, int newHeight)
+{
+	if ((newWidth >= 0) && (newHeight >= 0))
+	{
+		width = newWidth;
+		height = newHeight;
+	}
+}
+
+Mat ResizeFilter::ProcessImage(Mat image)
+{
+	Mat resized;
+	resize(image, resized, Size(width, height));
+	return resized;
+}
