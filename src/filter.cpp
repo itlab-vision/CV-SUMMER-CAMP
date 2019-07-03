dc5041564a06cd33b74b329384c1624c79f5f44d
sergei solovev <sergeisoly@gmail.com>
2019-07-04 00:44:16 +0300

    Update initialization

--- src/filter.cpp
+++ src/filter.cpp
@@ -1,14 +1,14 @@
 #include "filter.h"
 Mat GrayFilter::ProcessImage(Mat image){
     
-    Mat dst(image.size(), CV_8SC3);
+    Mat dst;
     cvtColor(image, dst, COLOR_BGR2GRAY);
     return dst;
     
 };
 
 Mat ResizeFilter::ProcessImage(Mat image){
-    Mat dst(image.size(), CV_8SC3);
+    Mat dst;
     resize(image, dst, cv::Size(width, height));
     return dst;
 };
