9ef4a1bee5a851d31ce1d7dac0bdfbf21fcbb8b2
summer school <temp2019@ccam.cmc.unn.net>
2019-07-03 17:03:13 +0300

    add resize gray

--- src/filter.cpp
+++ src/filter.cpp
@@ -1,1 +1,25 @@
 #include "filter.h"
+
+Mat GrayFilter::ProcessImage(Mat image)
+{
+    Mat res(image.size(), CV_8UC1);
+
+    // Filter image
+
+    cvtColor(image, res, COLOR_RGB2GRAY);
+   
+    return res;
+}
+
+ResizeFilter::ResizeFilter(int newWidth, int newHeight)
+{
+    width = newWidth;
+    height = newHeight;
+}
+
+Mat ResizeFilter::ProcessImage(Mat image)
+{
+    Mat res(image.size(), CV_8UC3);
+    resize(image, res, Size(width, height));
+    return res;
+}
