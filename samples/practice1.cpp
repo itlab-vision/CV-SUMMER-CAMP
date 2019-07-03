a61f7014d1c84334d3366dc0ef675ca96ea54173
summer school <temp2019@ccam.cmc.unn.net>
2019-07-03 16:44:08 +0300

    Can use programm in console

--- samples/practice1.cpp
+++ samples/practice1.cpp
@@ -14,6 +14,7 @@ const char* cmdAbout = "Sample of OpenCV usage. ";
 const char* cmdOptions =
 "{ i  image         | <none> | image to process        }"
 "{ w  width         | <none> | width for image resize  }"
+"{ f filter         | <none> | 0 - Gray 1 - Resize     }"
 "{ h  height        | <none> | height for image resize }"
 "{ q ? help usage   | <none> | print help message      }";
 
@@ -22,7 +23,7 @@ int main(int argc, char** argv)
     // Process input arguments
     CommandLineParser parser(argc, argv, cmdOptions);
     parser.about(cmdAbout);
-
+	int filter = 0;
     if (parser.has("help"))
     {
         parser.printMessage();
@@ -33,20 +34,32 @@ int main(int argc, char** argv)
         parser.printErrors();
         return 0;
     }
+	if (parser.has("filter")) {
+		filter = parser.get<int>("filter");
+	}
     
     // Load image
     String imgName(parser.get<String>("image"));
-	cv::Mat src,src1;
-	GrayFilter f;
-	ResizeFilter filt(parser.get<int>("width"),parser.get<int>("height"));
-	src = imread(imgName,1);
-	src1 = filt.ProcessImage(src);
+	cv::Mat src;
+	src = imread(imgName, 1);
+	switch (filter) {
+	case 0: {
+		GrayFilter f;
+		src = f.ProcessImage(src);
+		break;
+	}
+	case 1: {
+		ResizeFilter filt(parser.get<int>("width"), parser.get<int>("height"));
+		src = filt.ProcessImage(src);
+		break;
+	}
+	}
     
     // Filter image
 
 
     // Show image
-	cv::imshow("image", src1);
+	cv::imshow("image", src);
 	cv::waitKey();
     
     
