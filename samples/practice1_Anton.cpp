1f9583f8cc745d985f68f288b0a755bf7a1566ac
summer school <temp2019@ccam.cmc.unn.net>
2019-07-03 17:03:38 +0300

    Can use console to run project

--- samples/practice1_Anton.cpp
+++ samples/practice1_Anton.cpp
@@ -14,6 +14,7 @@ const char* cmdAbout = "Sample of OpenCV usage. ";
 const char* cmdOptions =
 "{ i  image         | <none> | image to process        }"
 "{ w  width         | <none> | width for image resize  }"
+"{ f filter         | <none> | 0 - Gray 1 - Resize     }"
 "{ h  height        | <none> | height for image resize }"
 "{ q ? help usage   | <none> | print help message      }";
 
@@ -22,7 +23,7 @@ int main(int argc, char** argv)
     // Process input arguments
     CommandLineParser parser(argc, argv, cmdOptions);
     parser.about(cmdAbout);
-
+	int filter = 0;
     if (parser.has("help"))
     {
         parser.printMessage();
@@ -33,17 +34,33 @@ int main(int argc, char** argv)
         parser.printErrors();
         return 0;
     }
+	if (parser.has("filter")) {
+		filter = parser.get<int>("filter");
+	}
     
     // Load image
     String imgName(parser.get<String>("image"));
-
+	cv::Mat src;
+	src = imread(imgName, 1);
+	
     
     // Filter image
 
-
+	switch (filter) {
+	case 0: {
+		GrayFilter f;
+		src = f.ProcessImage(src);
+		break;
+	}
+	case 1: {
+		ResizeFilter filt(parser.get<int>("width"), parser.get<int>("height"));
+		src = filt.ProcessImage(src);
+		break;
+	}
+	}
     // Show image
-    
-    
+	cv::imshow("image", src);
+	cv::waitKey();
     
     
     
