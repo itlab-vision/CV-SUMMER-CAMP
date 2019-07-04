1a1dfc43e6d897b818bb7009866915b5a5eae30c
summer school <temp2019@ccam.cmc.unn.net>
2019-07-04 18:11:23 +0300

    p2

--- samples/practice2.cpp
+++ samples/practice2.cpp
@@ -16,10 +16,10 @@ const char* cmdOptions =
 "{ i  image                             | <none> | image to process                  }"
 "{ w  width                             |        | image width for classification    }"
 "{ h  heigth                            |        | image heigth fro classification   }"
-"{ model_path                           |        | path to model                     }"
-"{ config_path                          |        | path to model configuration       }"
-"{ label_path                           |        | path to class labels              }"
-"{ mean                                 |        | vector of mean model values       }"
+"{ m model_path                         |        | path to model                     }"
+"{ c config_path                        |        | path to model configuration       }"
+"{ l label_path                         |        | path to class labels              }"
+"{ s scalar                             |        | vector of mean model values       }"
 "{ swap                                 |        | swap R and B channels. TRUE|FALSE }"
 "{ q ? help usage                       |        | print help message                }";
 
@@ -41,14 +41,35 @@ int main(int argc, char** argv)
 	}
 
 	// Load image and init parameters
-	String imgName(parser.get<String>("image"));
+	string imgName(parser.get<string>("image"));
+	float width(parser.get<float>("width"));
+	float heigth(parser.get<float>("heigth"));
+	string model(parser.get<string>("model_path"));
+	string config(parser.get<string>("config_path"));
+	string label(parser.get<string>("label_path"));
+	Scalar mean(parser.get<Scalar>("scalar"));
+	bool swap(parser.get<bool>("swap"));
 
 
 	//Image classification
-	
+
+	Mat image = imread(imgName);
+	double scale = 1;
+	mean = { 103.94,116.78,123.68 };
+	Classificator* dnnClassificator = new DnnClassificator(model, config, label, scale, width, heigth, mean, swap);
+	Mat prob = dnnClassificator->Classify(image);
+
+
+	Point classIdPoint;
+	double confidence;
+	minMaxLoc(prob.reshape(1, 1), 0, &confidence, 0, &classIdPoint);
+	int classId = classIdPoint.x;
+
 	
 	//Show result
 
+	std::cout << "Class: " << classId << '\n';
+	std::cout << "Confidence: " << confidence << '\n';
 
 	return 0;
 }
