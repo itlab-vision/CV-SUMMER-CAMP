51d8a24a461448decf9f48d66cf8033760046ba5
summer school <temp2019@ccam.cmc.unn.net>
2019-07-04 17:13:45 +0300

    add classify

--- samples/practice2.cpp
+++ samples/practice2.cpp
@@ -42,13 +42,32 @@ int main(int argc, char** argv)
 
 	// Load image and init parameters
 	String imgName(parser.get<String>("image"));
+    String model_path(parser.get<String>("model_path"));
+    String config_path(parser.get<String>("config_path"));
+    String path_label(parser.get<String>("label_path"));
+    int width(parser.get<int>("width"));
+    int height(parser.get<int>("heigth"));
+    Scalar mean(parser.get<Scalar>("mean"));
+    int swapRB(parser.get<int>("swap"));
 
+    DnnClassificator dnn(model_path, config_path, 
+      path_label, width , height, mean, swapRB);
 
+    Mat image = imread(imgName);
+    Mat prob;
 	//Image classification
-	
+    
+    prob = dnn.Classify(image);
 	
 	//Show result
+    Point classIdPoint;
+    double confidence;
+    minMaxLoc(prob.reshape(1, 1), 0, &confidence, 0, &classIdPoint);
 
+    int classId = classIdPoint.x;
+
+    std::cout << "Class: " << classId << '\n';
+    std::cout << "Confidence: " << confidence << '\n';
 
 	return 0;
 }
