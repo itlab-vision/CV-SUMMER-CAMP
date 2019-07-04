ccba8029b98b054c590900008e647f61ef9424e2
summer school <temp2019@ccam.cmc.unn.net>
2019-07-04 17:53:28 +0300

    Practice2

--- src/classificator.cpp
+++ src/classificator.cpp
@@ -1,1 +1,26 @@
 #include "classificator.h"
+
+DnnClassificator::DnnClassificator(string _pathToModel, string _pathToConfig, string _pathToLabels, int _inputWidth, int _inputHeight, Scalar _mean, bool _swapRB)
+{
+	pathToModel = _pathToModel;
+	pathToConfig = _pathToConfig;
+	pathToLabels = _pathToLabels;
+	inputWidth = _inputWidth;
+	inputHeight = _inputHeight;
+	mean = _mean;
+	swapRB = _swapRB;
+
+	net = readNet(pathToModel, pathToConfig);
+	net.setPreferableBackend(0);
+	net.setPreferableTarget(0);
+};
+Mat DnnClassificator::Classify(Mat frame)
+{
+	//Mat frame = imread(path_image);
+	Mat inputTensor;
+	blobFromImage(frame, inputTensor, 1, Size(inputWidth, inputHeight), mean, swapRB, false);
+	net.setInput(inputTensor);
+	Mat prob = net.forward();
+	prob.reshape(1, 1);
+	return prob;
+};
