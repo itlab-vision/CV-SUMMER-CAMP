98d992509e3afe3e5af3e3b4f71b70c0d1e65dc4
lindigas <52494607+lindigas@users.noreply.github.com>
2019-07-05 17:40:19 +0300

    Update practice2.cpp

--- samples/practice2.cpp
+++ samples/practice2.cpp
@@ -16,10 +16,10 @@ const char* cmdOptions =
 "{ i  image                             | <none> | image to process                  }"
 "{ w  width                             |        | image width for classification    }"
 "{ h  heigth                            |        | image heigth fro classification   }"
-"{ model_path                           |        | path to model                     }"
-"{ config_path                          |        | path to model configuration       }"
-"{ label_path                           |        | path to class labels              }"
-"{ mean                                 |        | vector of mean model values       }"
+"{ m model_path                         |        | path to model                     }"
+"{ c config_path                        |        | path to model configuration       }"
+"{ l label_path                         |        | path to class labels              }"
+"{ s scalar                             |        | vector of mean model values       }"
 "{ swap                                 |        | swap R and B channels. TRUE|FALSE }"
 "{ q ? help usage                       |        | print help message                }";
 
@@ -41,14 +41,47 @@ int main(int argc, char** argv)
 	}
 
 	// Load image and init parameters
-	String imgName(parser.get<String>("image"));
+	string imgName(parser.get<string>("image"));
+	float width(parser.get<float>("width"));
+	float heigth(parser.get<float>("heigth"));
+	string model(parser.get<string>("model_path"));
+	string config(parser.get<string>("config_path"));
+	string label(parser.get<string>("label_path"));
+	Scalar mean(parser.get<Scalar>("scalar"));
+	bool swap(parser.get<bool>("swap"));
 
 
 	//Image classification
-	
+
+	imgName = "C:/CV-Intel/CV-SUMMER-CAMP-build/caffe/space_shuttle.jpg"; //C:/CV-Intel/CV-SUMMER-CAMP-build/caffe
+	Mat image = imread(imgName);
+	//cv::namedWindow("My image", cv::WINDOW_NORMAL);
+	//cv::imshow("My image", image);
+	//cv::waitKey(0);
+	model = "C:/CV-Intel/CV-SUMMER-CAMP-build/caffe/squeezenet1.1.caffemodel";
+	config = "C:/CV-Intel/CV-SUMMER-CAMP-build/caffe/squeezenet1.1.prototxt";
+	label = "C:/CV-Intel/CV-SUMMER-CAMP-build/caffe/squeezenet1.1.labels";
+	double scale = 0.017;
+	width = 224;
+	heigth = 224;
+	mean = { 103.94,116.78,123.68 };
+	swap = false;
+	Classificator* dnnClassificator = new DnnClassificator(model, config, label, scale, width, heigth, mean, swap);
+	Mat prob = dnnClassificator->Classify(image);
+
+
+	Point classIdPoint;
+	double confidence;
+	minMaxLoc(prob.reshape(1, 1), 0, &confidence, 0, &classIdPoint);
+	int classId = classIdPoint.x;
+
+
 	
 	//Show result
 
+	std::cout << "Class: " << classId << '\n';
+	std::cout << "Confidence: " << confidence << '\n';
+	cv::waitKey(0);
 
 	return 0;
 }
