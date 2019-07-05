365cf9226bdb36c4010946fa9754bdfc5c5ef676
YoungMayne <ostrovsky1699@gmail.com>
2019-07-05 18:05:08 +0400

    practice-3

---
+++ samples/practice3_OSTROVSKY_ILYA.cpp
@@ -0,0 +1,81 @@
+#include <string>
+#include <iostream>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/dnn.hpp>
+
+#include "detector.h"
+
+using namespace std;
+using namespace cv;
+using namespace cv::dnn;
+
+const char* cmdAbout =
+"This is an empty application that can be treated as a template for your "
+"own doing-something-cool applications.";
+
+const char* cmdOptions =
+"{ i  image                             | <none> | image to process                  }"
+"{ w  width                             |        | image width for classification    }"
+"{ h  height                            |        | image heigth fro classification   }"
+"{ model_path                           |        | path to model                     }"
+"{ config_path                          |        | path to model configuration       }"
+"{ label_path                           |        | path to class labels              }"
+"{ mean                                 |        | vector of mean model values       }"
+"{ swap                                 |        | swap R and B channels. TRUE|FALSE }"
+"{ q ? help usage                       |        | print help message                }";
+
+
+void drawRects(Mat& image, const std::vector<DetectedObject>& objects, const std::string& name) {
+	for (size_t i = 0; i < objects.size(); ++i) {
+		if (objects[i].classname == name) {
+			rectangle(image, { objects[i].Left, objects[i].Top }, { objects[i].Right,  objects[i].Bottom }, { 0, 0, 255 });
+		}
+	}
+}
+
+
+int main(int argc, char** argv)
+{
+	// Process input arguments
+	CommandLineParser parser(argc, argv, cmdOptions);
+	parser.about(cmdAbout);
+
+	if (parser.has("help"))
+	{
+		parser.printMessage();
+		return 0;
+	}
+	if (!parser.check())
+	{
+		parser.printErrors();
+		return 0;
+	}
+
+	// Load image and init parameters
+	Mat image = imread(parser.get<String>("i"));
+
+	String ptm(parser.get<String>("model_path"));
+	String ptc(parser.get<String>("config_path"));
+	String ptl(parser.get<String>("label_path"));
+
+	uint32_t width = parser.get<int>("width");
+	uint32_t height = parser.get<int>("height");
+
+	Scalar mean = parser.get<Scalar>("mean");
+	bool swapRB = parser.get<bool>("swap");
+
+	//Image classification
+
+	DnnDetector detector(ptm, ptc, ptl);
+	auto detected = detector.Detect(image, { (int)width, (int)height }, mean, swapRB);
+	drawRects(image, detected, "person");
+
+	//Show result
+	namedWindow("window");
+	imshow("window", image);
+
+	waitKey(0);
+	return 0;
+}
--- src/detector.cpp
+++ src/detector.cpp
@@ -1,1 +1,53 @@
 #include "detector.h"
+
+#include <fstream>
+#include <string>
+
+
+std::string readLabel(const String& path, int number) {
+	std::ifstream is(path);
+	std::string temp;
+	std::vector<std::string> result;
+	while (std::getline(is, temp)) {
+		if (temp.size()) {
+			result.push_back(temp);
+		}
+	}
+	return number < result.size() ? result[number] : "Undef";
+}
+
+
+
+DnnDetector::DnnDetector(const String & ptm, const String & ptc, const String& ptl) {
+	net = readNet(ptm, ptc);
+	this->ptl = ptl;
+
+	net.setPreferableBackend(0);
+	net.setPreferableTarget(0);
+}
+
+
+vector<DetectedObject> DnnDetector::Detect(Mat image, Size size, Scalar mean, bool swapRB, double scale) {
+	ResizeFilter resized(size.width, size.height);
+
+	net.setInput(blobFromImage(resized.ProcessImage(image), scale, size, mean, swapRB));
+
+	Mat reshaped = net.forward().reshape(1, 100);
+
+	vector<DetectedObject> result;
+	for (int i = 0; i < reshaped.rows; ++i) {
+		uchar* ptr = reshaped.data + i * reshaped.step;
+
+		DetectedObject temp;		
+		temp.classname = readLabel(ptl, ptr[1]);
+		temp.uuid = ptr[1];
+		temp.Left = ptr[3];
+		temp.Top = ptr[6];
+		temp.Right = ptr[5];
+		temp.Bottom = ptr[4];
+
+		result.push_back(temp);
+	}
+
+	return result;
+}
