3cfd1fb0dcc26a349e0f205da12551c0768d370b
sergei solovev <sergeisoly@gmail.com>
2019-07-07 02:12:19 +0300

    practice3 main part is done

--- include/detector.h
+++ include/detector.h
@@ -15,5 +15,23 @@ using namespace std;
 class Detector
 {
 public:
-    virtual vector<DetectedObject> Detect(Mat image) = 0 {}
+    virtual vector<DetectedObject> Detect(Mat image) = 0;
+};
+
+class DnnDetector: public Detector
+{
+    vector<string> classes;
+    string model, config;
+    int inputHeight, inputWidth;
+    Scalar mean;
+    int backendId, targetId;
+    bool swapRB;
+    Net net;
+    double scale;
+    //double confThreshold = 0.5;
+    
+public:
+    DnnDetector(vector<string> classes, string model, string config, int inputWidth = 300, int inputHeight = 300, bool swapRB = false, double scale = 0.007843, Scalar mean = {127.5,127.5,127.5}, int backendId = DNN_BACKEND_OPENCV, int targetId = DNN_TARGET_CPU);
+    
+    vector<DetectedObject> Detect(Mat image);
 };
