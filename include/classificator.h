25bb27800a97df6d77836589b004818a0dc08954
Zercon <zercon@list.ru>
2019-07-06 17:47:19 +0300

    practice 1 + practice 2

--- include/classificator.h
+++ include/classificator.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 #include <opencv2/dnn.hpp>
 #include <opencv2/imgproc.hpp>
@@ -16,4 +17,21 @@ class Classificator
 public:
     vector<string> classesNames;
     virtual Mat Classify(Mat image) = 0 {}
+};
+
+class DnnClassificator : public Classificator
+{
+public:
+	DnnClassificator(String path_to_model, String path_to_config, String path_to_labels, int inputWidth, int inputHeight, Scalar mean = (0,0,0,0), bool swapRB=false);
+	Mat Classify(Mat image);
+	~DnnClassificator() {};
+
+private:
+	String path_to_model;
+	String path_to_config;
+	String path_to_labels;
+	int inputWidth;
+	int inputHeight;
+	Scalar mean;
+	bool swapRB;
 };
--- src/classificator.cpp
+++ src/classificator.cpp
@@ -1,1 +1,17 @@
 #include "classificator.h"
+
+DnnClassificator::DnnClassificator(String path_to_model, String path_to_config, String path_to_labels, int inputWidth, int inputHeight, Scalar mean, bool swapRB)
+{
+	this->path_to_model = path_to_model;
+	this->path_to_config = path_to_config;
+	this->path_to_labels = path_to_labels;
+	this->inputWidth = inputWidth;
+	this->inputHeight = inputHeight;
+	this->mean = mean;
+	this->swapRB = swapRB;
+}
+
+Mat DnnClassificator::Classify(Mat image)
+{
+	return Mat();
+}
