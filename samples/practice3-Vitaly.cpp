03b872c867eff82ced718ea5db38c825ea4fa37b
Vitaly Dobrohotov <19dobrohotov99@mail.ru>
2019-07-08 01:22:18 +0300

    remake main, all workinggit add .!

--- samples/practice3-Vitaly.cpp
+++ samples/practice3-Vitaly.cpp
@@ -1,6 +1,6 @@
 #include <string>
 #include <iostream>
-
+#include <fstream>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/dnn.hpp>
@@ -61,17 +61,30 @@ int main(int argc, const char** argv) {
 	DnnDetector  DnnDet(model, config, label, width, height, mean, scale, swapRB);
 
 	vector <DetectedObject> detObj = DnnDet.Detect(src);
-	for (int i = 0; i < detObj.size(); i++)
+	string labels[21];
+	ifstream file (label);
+	int i = 0;
+	while (!file.eof())
 	{
-		Point leftbottom(detObj.back().Left, detObj.back().Right);
-		Point righttop(detObj.back().Top, detObj.back().Bottom);
-		Rect box(leftbottom, righttop);
-		rectangle(src, box, Scalar(65, 105, 225), 1, 1, 0);
-
-		cout << "Class: " << to_string(detObj.back().uuid)
-			<< " " << detObj.back().classname << endl;
-		cout << "Confidence: " << to_string(detObj.back().confidence) << endl;
+		getline(file, labels[i]);
+		i++;
+	}
+	while (!detObj.empty()) {
+		string objClass = "Class: "  + labels[detObj.back().uuid];
+		string conf = "Confidence: " + to_string(detObj.back().confidence);
+		Point leftbottom(detObj.back().Left, detObj.back().Bottom);
+		Point righttop(detObj.back().Right, detObj.back().Top
+		);
 		cout << leftbottom << " " << righttop << endl;
+		Rect box(leftbottom, righttop);
+		cout << objClass << " " << conf << endl;
+		rectangle(src, box, Scalar(0, 255, 0), 2, 1, 0);
+
+		putText(src, objClass, Size(detObj.back().Left , detObj.back().Bottom-17), FONT_HERSHEY_COMPLEX_SMALL, 1,
+			Scalar(0, 255, 0), 1, 0);
+		putText(src, conf, Size(detObj.back().Left , detObj.back().Bottom-3), FONT_HERSHEY_COMPLEX_SMALL, 1,
+			Scalar(0, 255, 0), 1, 0);
+		detObj.pop_back();
 	}
 	imshow("Detected", src);
 	waitKey(0);
