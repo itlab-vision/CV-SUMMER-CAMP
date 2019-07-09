f23a8a73ec45a3bf204509c5763f105e2e893bd0
ls-pepper <v.pepper@inbox.ru>
2019-07-10 00:00:51 +0300

    practice 3 done

--- include/detector.h
+++ include/detector.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <fstream>
 
 #include <opencv2/dnn.hpp>
 #include <opencv2/imgproc.hpp>
@@ -16,4 +17,22 @@ class Detector
 {
 public:
     virtual vector<DetectedObject> Detect(Mat image) = 0 {}
+	virtual string ParseLabels(int class_id) = 0 {}
+};
+
+class DnnDetector : public Detector {
+private:
+	string path_to_model;
+	string path_to_config;
+	string path_to_labels;
+	int input_width = 300;
+	int input_height = 300;
+	double scale = 0.007843;
+	Scalar mean = Scalar(127.5, 127.5, 127.5);;
+	bool swapRB = false;
+	Net net;
+public:
+	DnnDetector(string path_to_model, string path_to_config, string path_to_labels);
+	vector<DetectedObject> Detect(Mat image);
+	string ParseLabels(int class_id);
 };
--- samples/practice3.cpp
+++ samples/practice3.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include "detector.h"
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
@@ -10,27 +11,61 @@ using namespace cv;
 using namespace cv::dnn;
 
 const char* cmdAbout =
-    "This is an empty application that can be treated as a template for your "
-    "own doing-something-cool applications.";
+"This is an empty application that can be treated as a template for your "
+"own doing-something-cool applications.";
 
 const char* cmdOptions =
-    "{ i image        |        | image to process         }"
-    "{ h ? help usage |        | print help message       }";
+"{ i  image                             | <none> | image to process                  }"
+"{ m  model_path                           |        | path to model                     }"
+"{ c  config_path                          |        | path to model configuration       }"
+"{ l  label_path                           |        | path to class labels              }"
+"{ h ? help usage                       |        | print help message                }";
 
 
 int main(int argc, const char** argv) {
-  // Parse command line arguments.
-  CommandLineParser parser(argc, argv, cmdOptions);
-  parser.about(cmdAbout);
+	// Parse command line arguments.
+	CommandLineParser parser(argc, argv, cmdOptions);
+	parser.about(cmdAbout);
 
-  // If help option is given, print help message and exit.
-  if (parser.get<bool>("help")) {
-    parser.printMessage();
-    return 0;
-  }
+	// If help option is given, print help message and exit.
+	if (parser.get<bool>("help")) {
+		parser.printMessage();
+		return 0;
+	}
 
-  // Do something cool.
-  cout << "This is empty template sample." << endl;
+	string imgName(parser.get<string>("image"));
+	string model_path(parser.get<string>("model_path"));
+	string config_path(parser.get<string>("config_path"));
+	string label_path(parser.get<string>("label_path"));
+	Mat image = imread(imgName);
 
-  return 0;
+	Detector* dnn = new DnnDetector(model_path, config_path, label_path);
+	vector<DetectedObject> vec = dnn->Detect(image);
+	for (int i = 0; i < vec.size(); i++) 
+	{
+		Point p1(vec[i].Left, vec[i].Bottom);
+		Point p2(vec[i].Right, vec[i].Top);
+
+		Rect rect(p1, p2);
+		rectangle(image, rect, Scalar(0, 255, 0), 2);
+
+		string text = "Class: " + vec[i].className + "Score: " + to_string(vec[i].confidence);
+
+		putText(image,text,p1, FONT_HERSHEY_DUPLEX, 0.5, Scalar(0, 255, 0), 1, 8);
+
+
+
+		cout << "Class: " << vec[i].className << endl;
+		cout << "Confidence: " << vec[i].confidence << endl;
+		cout << p1 << " " << p2 << endl;
+		
+
+	}
+	
+
+	imshow("Detected", image);
+	waitKey(0);
+
+	delete dnn;
+	return 0;
 }
